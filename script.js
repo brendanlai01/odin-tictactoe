edffd117b75a9aa9c4715cadc63dfcb1b63fa63d
Brendan <103078608+brendanlai01@users.noreply.github.com>
2024-07-16 00:58:37 -0400

    change array to empty

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameboard = (function() {
-    let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    let board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
     const WINNING_COMBINATIONS = [
         [0, 1, 2],
         [3, 4, 5],
